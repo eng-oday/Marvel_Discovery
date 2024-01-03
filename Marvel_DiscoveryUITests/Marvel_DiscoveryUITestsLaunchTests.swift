486f084deb436d25469c49d695c476012440d3ae
eng-oday <odaydieg@gmail.com>
2024-01-03 20:33:33 +0200

    welcome to marvel👽

---
+++ Marvel_DiscoveryUITests/Marvel_DiscoveryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Marvel_DiscoveryUITestsLaunchTests.swift
+//  Marvel_DiscoveryUITests
+//
+//  Created by eng-oday on 03/01/2024.
+//
+
+import XCTest
+
+final class Marvel_DiscoveryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
